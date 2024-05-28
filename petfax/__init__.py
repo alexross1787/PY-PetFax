5d3ac339ca57602e76cf553c247a2c5e0ca14bb6
alexross1787 <alexross1787@gmail.com>
2024-05-27 19:03:23 -0700

    finished assignment

---
+++ petfax/__init__.py
@@ -0,0 +1,20 @@
+# The Application Factory
+
+# imports
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+    
+    @app.route('/')
+    def hello():
+        return 'Hello, Petfax'
+    
+    #register pet blueprint
+    from . import pet
+    app.register_blueprint(pet.bp)
+
+    from . import facts
+    app.register_blueprint(facts.bp)
+
+    return app
